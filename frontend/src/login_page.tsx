db990f27b3a9337d081abc7722f84a50db887fe5
Andy Cai <andycai20000212@gmail.com>
2020-04-24 22:25:58 -0700

    Created simple login page

---
+++ frontend/src/login_page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Button from '@material-ui/core/Button'
+
+export default class MyButton extends React.Component {
+    render() {
+        return (
+            <a>
+                <Button variant="contained" color="primary" disableElevation>
+                    Login
+                </Button> 
+
+                <Button variant="contained" color="primary" disableElevation>
+                    Sign Up 
+                </Button> 
+            </a>
+        )
+    }
+}
+
