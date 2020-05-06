99d641d4150f1968b5a6471d35e10c8ede199262
Andy Cai <andycai20000212@gmail.com>
2020-05-06 01:50:43 -0700

    Added express w Firebase

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-const 
-    express = require('express'),
-    app = express(),
-    port = 3000;
-
-app.get('/', (req, res) => res.send('Arch 👼!'))
-
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const { db } = require('./util/admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+app.get("/helloWorld", (req, res) => {
+    res.send("Hello from ArchAngel!");
 });
+
+app.get("/startups/:id", (req, res) => {
+    
+});
+
+//base url: https://baseurl.com/api
+
+exports.api = functions.https.onRequest(app);
