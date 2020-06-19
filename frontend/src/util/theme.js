ed721fbae3d140af44c6c897a41e4501edf14732
stephanie-phuong-khanh <stephaniekdoan@gmail.com>
2020-06-18 22:54:11 -0700

    auth done

---
+++ frontend/src/util/theme.js
@@ -0,0 +1,37 @@
+export default {
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "#b22a00",
+      contrastText: "#fff",
+    },
+  },
+  signin: {
+    form: {
+      textAlign: "center",
+    },
+    pageTitle: {
+      margin: "20px auto 20px auto",
+    },
+    textField: {
+      margin: "20px auto 20px auto",
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+    },
+    button: {
+      position: "relative",
+    },
+    progress: {
+      position: "absolute",
+    },
+  },
+};
